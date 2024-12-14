478f238080c6cc166a18f22056b9a37659cf149b
luanmvcosta0 <luanmvcosta@mgail.com>
2024-12-14 13:00:10 -0300

    Apredendo componentes em React

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Titulo from "./Titulo"
+
+function App() {
+  return (
+    <div>
+      <Titulo/>
+      <Titulo/>
+      <Titulo/>
+      <Titulo/>
+      <Titulo/>
+    </div>
+  ) 
+}
+
+export default App
---
+++ src/Titulo.jsx
@@ -0,0 +1,11 @@
+function Titulo({ nome }) {
+
+    return (
+        <div>
+            <h1>Oi eu sou {nome}</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur ab commodi dolores placeat facilis non distinctio ut ipsa nesciunt! Inventore repellat eveniet minima. Voluptate enim sed illum asperiores iusto.</p>
+        </div>
+    )
+}
+
+export default Titulo
